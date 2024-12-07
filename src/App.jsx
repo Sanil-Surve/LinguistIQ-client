fdba3332dc8e3e25a414428e3d6b73cf6d751e07
Sanil-Surve <sanilsurve42@gmail.com>
2024-12-07 09:28:04 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { useSelector } from "react-redux";
 import Navbar from "./components/Navbar";
-import Unauthorized from "./components/Unauthorized";
-import Home from "./components/Home";
+
+const Unauthorized = lazy(() => import("./components/Unauthorized"));
+const Home = lazy(() => import("./components/Home"));
 
 function App() {
-
   const user = useSelector((state) => state.auth.user);
 
   return (
     <div>
       <Navbar />
-
-      {user ? (
-        <Home />
-      ) : (
-        <Unauthorized />
-      )}
+      <Suspense
+        fallback={
+          <div style={loaderStyle}>
+            <div className="spinner"></div>
+          </div>
+        }
+      >
+        {user ? <Home /> : <Unauthorized />}
+      </Suspense>
     </div>
   );
 }
 
 export default App;
+
+const loaderStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh", // Full height of the viewport
+  backgroundColor: "#f9f9f9", 
+};
