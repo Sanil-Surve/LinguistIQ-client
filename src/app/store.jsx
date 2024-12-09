c385f92a3b335d193363175ab28bfaf39631924f
Sanil-Surve <sanilsurve42@gmail.com>
2024-12-09 12:40:17 +0530

    State Data Stored in LocalStorage

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,13 +1,48 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import authReducer from "./slices/authSlice";
+// import lessonReducer from "./slices/lessonSlice";
+// import quizReducer from "./slices/quizSlice";
+
+// export const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//     lesson: lessonReducer,
+//     quiz: quizReducer,
+//   },
+// });
+
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // Defaults to localStorage for web
+import { combineReducers } from "redux";
 import authReducer from "./slices/authSlice";
 import lessonReducer from "./slices/lessonSlice";
 import quizReducer from "./slices/quizSlice";
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    lesson: lessonReducer,
-    quiz: quizReducer,
-  },
+// Combine reducers
+const rootReducer = combineReducers({
+  auth: authReducer,
+  lesson: lessonReducer,
+  quiz: quizReducer,
 });
 
+// Persist configuration
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// Persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Disable serializable check for redux-persist
+    }),
+});
+
+// Persistor
+export const persistor = persistStore(store);
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,27 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './app/store.jsx'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+// import { Provider } from 'react-redux'
+// import { store } from './app/store.jsx'
 
-createRoot(document.getElementById('root')).render(
-   <Provider store={store}>
-     <App />
-   </Provider>
-)
+// createRoot(document.getElementById('root')).render(
+//    <Provider store={store}>
+//      <App />
+//    </Provider>
+// )
+
+
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store, persistor } from "./app/store.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
